d345c76821d1619a5212cc0b06004d8f56e6dfab
kinuthia lawrence <kinuthialawrece343@gmail.com>
2024-03-01 07:37:54 +0300

    fixed all issues in app.js

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,9 @@ var logger = require('morgan');
 const bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const connectDb = require('./public/javascripts/databaseConn');
-var submit = require("./routes/bookNowSubmitRoute");
-var cors = require('cors');
+const connectDb = require('./public/javascripts/databaseConn');/// imported database connection
+var submit = require("./routes/bookNowSubmitRoute"); // imported the booknow route
+var cors = require('cors'); // imported cors
 
 var app = express();
 connectDb();
@@ -17,19 +17,32 @@ connectDb();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(bodyParser.json); //added body parser
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// using cors options to connect to the server
+const corsOptions ={
+  origin : "http://127.0.0.1:5500",
+  methods : "POST",
+  optionSuccessStatus : 204,
+};
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//my routes
+app.options("/submit", cors(corsOptions));
+app.post("/submit",cors(corsOptions), submit);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
 
 // error handler
 app.use(function(err, req, res, next) {
